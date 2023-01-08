6179ee3e4e45ef7d4fa028c136617f085cf46ca2
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-01-08 17:28:28 -0300

    versao JS 4.0
    Arquitetura dos botoes completa, funcional

--- script/script.js
+++ script/script.js
@@ -9,21 +9,90 @@ function desmarcaBotaoAnterior(seletor){
     }
 }
 
+let pratoPedido;
+let precoPratoPedido;
+
 function selecionarPrato(containerClicado) {
 	let seletor = ".prato";
-	desmarcaBotaoAnterior(seletor);
-	containerClicado.classList.toggle("selected");
+
+	if (pratoPedido === undefined && precoPratoPedido === undefined) {
+		containerClicado.classList.add("selected");
+		pratoPedido = document.querySelector(".prato.selected .nomePrato").innerHTML;
+		precoPratoPedido = document.querySelector(".prato.selected .precoPrato").innerHTML;
+	} else {
+		if (containerClicado.classList.contains("selected")) {
+			containerClicado.classList.remove("selected");
+			pratoPedido = undefined;
+			precoPratoPedido = undefined;
+		} else {
+			desmarcaBotaoAnterior(seletor);
+			containerClicado.classList.add("selected");
+			pratoPedido = document.querySelector(".prato.selected .nomePrato").innerHTML;
+			precoPratoPedido = document.querySelector(".prato.selected .precoPrato").innerHTML;
+		}
+	}
+
+	verificarPedido()
 }
+
+let bebidaPedido;
+let precoBebidaPedido;
 
 function selecionarBebida(containerClicado) {
 	let seletor = ".bebida";
-	desmarcaBotaoAnterior(seletor);
-	containerClicado.classList.add("selected");
+
+	if (bebidaPedido === undefined && precoBebidaPedido === undefined) {
+		containerClicado.classList.add("selected");
+		bebidaPedido = document.querySelector(".bebida.selected .nomeBebida").innerHTML;
+		precoBebidaPedido = document.querySelector(".bebida.selected .precoBebida").innerHTML;
+	} else {
+		if (containerClicado.classList.contains("selected")) {
+			containerClicado.classList.remove("selected");
+			bebidaPedido = undefined;
+			precoBebidaPedido = undefined;
+		} else {
+			desmarcaBotaoAnterior(seletor);
+			containerClicado.classList.add("selected");
+			bebidaPedido = document.querySelector(".bebida.selected .nomeBebida").innerHTML;
+			precoBebidaPedido = document.querySelector(".bebida.selected .precoBebida").innerHTML;
+		}
+	}
+	
+	verificarPedido()
 }
+
+let sobremesaPedido;
+let precoSobremesaPedido;
 
 function selecionarSobremesa(containerClicado) {
 	let seletor = ".sobremesa";
-	desmarcaBotaoAnterior(seletor);
-	containerClicado.classList.toggle("selected");
+
+	if (sobremesaPedido === undefined && precoSobremesaPedido === undefined) {
+		containerClicado.classList.add("selected");
+		sobremesaPedido = document.querySelector(".sobremesa.selected .nomeSobremesa").innerHTML;
+		precoSobremesaPedido = document.querySelector(".sobremesa.selected .precoSobremesa").innerHTML;
+	} else {
+		if (containerClicado.classList.contains("selected")) {
+			containerClicado.classList.remove("selected");
+			sobremesaPedido = undefined;
+			precoSobremesaPedido = undefined;
+		} else {
+			desmarcaBotaoAnterior(seletor);
+			containerClicado.classList.add("selected");
+			sobremesaPedido = document.querySelector(".sobremesa.selected .nomeSobremesa").innerHTML;
+			precoSobremesaPedido = document.querySelector(".sobremesa.selected .precoSobremesa").innerHTML;
+		}
+	}
+	
+	verificarPedido()
 }
 
+function verificarPedido() {
+	if (pratoPedido !== undefined && bebidaPedido !== undefined && sobremesaPedido !== undefined) {
+		document.querySelector(".botao-confirmar-pedido").disabled = false;
+		document.querySelector(".botao-confirmar-pedido").innerHTML = "Fechar pedido";
+	} else {
+		document.querySelector(".botao-confirmar-pedido").disabled = true;
+		document.querySelector(".botao-confirmar-pedido").innerHTML = "Selecione os 3 itens <br>para fechar o pedido";
+	}
+}
