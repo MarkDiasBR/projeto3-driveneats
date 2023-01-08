d0e296fa6f44bd6e2415cda6b6e649653c820586
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-01-08 14:58:40 -0300

    Versão JS 2.0
    seletores prontos

--- script/script.js
+++ script/script.js
@@ -1,4 +1,29 @@
-function selecionarPrato(argumento) {
-	const containerPrato = argumento;
-	containerPrato.classList.toggle("selected");
+function desmarcaBotaoAnterior(seletor){
+    // pegar o botão que foi selecionado anteriormente
+    const botaoSelecionadoAnteriormente = document.querySelector(`${seletor}.selected`);
+
+    // anter de remover a classe selecionado, verificar se o elemento não é null
+    if (botaoSelecionadoAnteriormente !== null){
+        // remover a classe selected desse botao
+        botaoSelecionadoAnteriormente.classList.remove('selected');
+    }
 }
+
+function selecionarPrato(containerClicado) {
+	let seletor = ".prato";
+	desmarcaBotaoAnterior(seletor);
+	containerClicado.classList.toggle("selected");
+}
+
+function selecionarBebida(containerClicado) {
+	let seletor = ".bebida";
+	desmarcaBotaoAnterior(seletor);
+	containerClicado.classList.add("selected");
+}
+
+function selecionarSobremesa(containerClicado) {
+	let seletor = ".sobremesa";
+	desmarcaBotaoAnterior(seletor);
+	containerClicado.classList.toggle("selected");
+}
+
